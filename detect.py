93547316ed7dad8e7276f82bb1f6b34d9594867a
nhannah044 <nhannah@gmu.edu>
2024-03-26 20:26:55 -0400

    testing push

--- detect.py
+++ detect.py
@@ -3,3 +3,5 @@ This file contains code for the object detection portion of project birdbrain.
 It will detect birds using a YoloV8 model trained on a custom dataset.
 {https://github.com/yangyangtiaoguo/GBDD-1433-2023}
 """
+
+pass
